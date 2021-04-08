14fdd2a4cc043d6c865530fd78b5a1ec890db7b4
Sara <sara.m.adamski@gmail.com>
2021-04-08 11:45:47 -0500

    whichChars function

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,43 @@
-var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,!@#$%^&*?';
+var characters = 'abcdefghijklmnopqrstuvwxyz' + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + '0123456789' + '.,!@#$%^&*?';
 
-function generatePassword(length) {
-    var result = [];
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+var length = window.prompt("Choose a password length between 8 and 128 characters:");
+
+var lower = window.confirm("Would you like your password to contain lowercase letters?");
+var upper = window.confirm("Would you like your password to contain uppercase letters?");
+var nums = window.confirm("Would you like your password to contain numbers?");
+var special = window.confirm("Would you like your password to contain special characters?");
+
+function whichChars() {
+  if (lower === true) {
+    console.log("confirm lowercase");
+  } else {
+    characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + '0123456789' + '.,!@#$%^&*?';
+    if (upper === true) {
+    console.log("confirm uppercase");
+    } else {
+      characters = '0123456789' + '.,!@#$%^&*?';
+      if (nums === true) {
+        console.log("confirm numbers");
+      } else {
+        characters = '.,!@#$%^&*?';
+        if (special === true) {
+          console.log("confirm special characters");
+        } else {
+          alert("Your password must contain characters!");
+        }
+      }
     }
-    return result;
+  }
 }
-console.log(generatePassword(8));
 
-// var howLong = window.prompt("Choose a password length between 8 and 128 characters:");
-// var lower = window.confirm("Would you like your password to contain lowercase letters?");
-// var upper = window.confirm("Would you like your password to contain uppercase letters?");
-// var nums = window.confirm("Would you like your password to contain numbers?");
-// var special = window.confirm("Would you like your password to contain special characters?");
-
-// var generatePassword = function () {
-  
-// }
+function generatePassword() {
+  // var characters = whichChars();
+  var result = [];
+  for (var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -27,10 +46,9 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+console.log(generatePassword(length));
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
