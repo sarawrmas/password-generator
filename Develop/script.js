aad4285d9ec870e15d874e71a994c085592dde4d
Sara <sara.m.adamski@gmail.com>
2021-04-09 12:57:49 -0500

    troubleshoot password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,42 +1,16 @@
-var characters = 'abcdefghijklmnopqrstuvwxyz' + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + '0123456789' + '.,!@#$%^&*?';
+var characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!@#$%^&*?';
 
-function prompts() {
-  var characters = 'abcdefghijklmnopqrstuvwxyz' + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + '0123456789' + '.,!@#$%^&*?';
-  var length = window.prompt("Choose a password length between 8 and 128 characters:");
-    if (!length || length < 8) {
-      alert("Please pick a number between 8 and 128");
-      return prompts();
-    }
-  var lower = window.confirm("Would you like your password to contain lowercase letters?");
-    if (lower === true) {
-      console.log("Confirm lowercase");
-    } else {
-      console.log("Deny lowercase");
-    }
-  var upper = window.confirm("Would you like your password to contain uppercase letters?");
-    if (upper === true) {
-      console.log("Confirm uppercase");
-    } else {
-      console.log("Deny uppercase");
-    }
-  var nums = window.confirm("Would you like your password to contain numbers?");
-    if (nums === true) {
-      console.log("Confirm numbers");
-    } else {
-      console.log("Deny numbers");
-    }
-  var special = window.confirm("Would you like your password to contain special characters?");
-    if (special === true) {
-      console.log("Confirm special characters");
-    } else {
-      console.log("Deny special characters");
-    }
-}
+var length = window.prompt("Choose a password length between 8 and 128 characters:");
+// TO DO: reload if doesn't meet criteria
+var lower = window.confirm("Would you like your password to contain lowercase letters?");
+var upper = window.confirm("Would you like your password to contain uppercase letters?");
+var nums = window.confirm("Would you like your password to contain numbers?");
+var special = window.confirm("Would you like your password to contain special characters?");
+var generate = window.alert("Criteria set. Please press Generate button to get your password!");
 
-prompts();
+// TO DO: generate password using criteria
 
-function generatePassword(whichChars) {
-  // var characters = whichChars();
+function generatePassword() {
   var result = [];
   for (var i = 0; i < length; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * characters.length));
@@ -53,7 +27,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-console.log(generatePassword());
+
+console.log(generatePassword(length));
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
