4a48408fd4edd9b762cfc5340717ea962667bef5
Sara <sara.m.adamski@gmail.com>
2021-04-11 11:32:54 -0500

    generate password using criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,59 @@
-var characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!@#$%^&*?';
+function lengthConfirm() {
+  var length = prompt("Choose a password length between 8 and 128 characters:");
+  if (!length || length < 8 || length > 128) {
+    alert("Please pick correct password length");
+    return lengthConfirm();
+  }
+  return length;
+}
 
-var length = window.prompt("Choose a password length between 8 and 128 characters:");
+var length = lengthConfirm();
+
 // TO DO: reload if doesn't meet criteria
-var lower = window.confirm("Would you like your password to contain lowercase letters?");
-var upper = window.confirm("Would you like your password to contain uppercase letters?");
-var nums = window.confirm("Would you like your password to contain numbers?");
-var special = window.confirm("Would you like your password to contain special characters?");
+var askLower = window.confirm("Would you like your password to contain lowercase letters?");
+  if(askLower === true) {
+    var confirmLower ='abcdefghijklmnopqrstuvwxyz';
+  } else {
+    var confirmLower = '';
+  }
+var askUpper = window.confirm("Would you like your password to contain uppercase letters?");
+  if(askUpper === true) {
+    var confirmUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  } else {
+    var confirmUpper = '';
+  }
+var askNums = window.confirm("Would you like your password to contain numbers?");
+  if(askNums === true) {
+    var confirmNums = '0123456789';
+  } else {
+    var confirmNums = '';
+  }
+var askSpecial = window.confirm("Would you like your password to contain special characters?");
+  if(askSpecial === true) {
+    var confirmSpecial = '.,!@#$%^&*?';
+} else {
+  var confirmSpecial = '';
+}
+
 var generate = window.alert("Criteria set. Please press Generate button to get your password!");
 
 // TO DO: generate password using criteria
 
+var characters =
+  'abcdefghijklmnopqrstuvwxyz' + 
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+  '0123456789' +
+  '.,!@#$%^&*?';
+
 function generatePassword() {
-  var result = [];
-  for (var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
+  var characters = (confirmLower + confirmUpper + confirmNums + confirmSpecial);
+  var randomPassword = [];
+  for (var i = 0; i < length; i++) {
+    var rnum = Math.floor(Math.random() * characters.length);
+    randomPassword += characters.substring(rnum, rnum+1);
   }
-  return result;
+  return randomPassword;
+  console.log(randomPassword);
 }
 
 // Get references to the #generate element
@@ -28,7 +66,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
-console.log(generatePassword(length));
+console.log(generatePassword());
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
